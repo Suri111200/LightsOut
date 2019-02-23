a148ea68104b6a820b04d7fcdac29c1dc3282e59
Hrithik Shah <hrithikchamp@gmail.com>
2019-02-22 23:37:31 -0500

    Initial commit

---
+++ 01/LightsOut.java
@@ -0,0 +1,112 @@
+import java.util.ArrayList;
+import java.util.Queue;
+
+
+/*
+ * The class <b>LightsOut</b> is the
+ * class that implements the method to
+ * computs solutions of the Lights Out game.
+ * It contains the main of our application.
+ *
+ * @author GVJ, Hrithik, Soorya at uOttawa
+ */
+
+public class LightsOut {
+
+    // Your variables here
+    static ArrayListSolutionQueue partialSolutions; 
+    //static ArrayList<Solution> solutions = new ArrayList<>();
+
+    /**
+     * The method <b>solve</b> finds all the 
+     * solutions to the <b>Lights Out</b> game 
+     * for an initially completely ``off'' board 
+     * of size <b>widthxheight</b>, using a  
+     * Breadth-First Search algorithm. 
+     *
+     * It returns an <b>ArrayList&lt;Solution&gt;</b> 
+     * containing all the valid solutions to the 
+     * problem.
+     *
+     * During the computation of the solution, the 
+     * method prints out a message each time a new 
+     * solution  is found, along with the total time 
+     * it took (in milliseconds) to find that solution.
+     *
+     * @param width
+     *  the width of the board
+     * @param height
+     *  the height of the board
+     * @return
+     *  an instance of <b>ArrayList&lt;Solution&gt;</b>
+     * containing all the solutions
+     */
+    public static ArrayList<Solution> solve(int width, int height){
+
+        //Your code here
+        partialSolutions = new ArrayListSolutionQueue();
+        ArrayList<Solution> solutions = new ArrayList<>();
+
+        partialSolutions.enqueue(new Solution(width, height));
+
+        while (!(partialSolutions.isEmpty()))
+        {
+            Solution current = partialSolutions.dequeue();
+
+            if (current.isReady() && current.isSuccessful())
+            {
+                solutions.add(current);
+                System.out.println ("New solution found "+ System.nanoTime());
+            }
+            else
+            {
+                Solution current1 = new Solution (current);
+                Solution current2 = new Solution (current);
+
+                current1.setNext(true);
+                current2.setNext(false);
+
+                solutions.add(current1);
+                solutions.add(current2);
+            }
+        }
+        return solutions;        
+    }
+
+    /**
+     * <b>main</b> method  calls the method <b>solve</b> 
+     * and then prints out the number of solutions found,
+     * as well as the details of each solution.
+     *
+     * The <b>width</b> and <b>height</b> used by the 
+     * main are passed as runtime parameters to
+     * the program. If no runtime parameters are passed 
+     * to the program, or if the parameters are incorrect,
+     * then the default values are used.
+     *
+     * @param args
+     *  Strings array of runtime parameters
+     */
+    public static void main(String[] args) {
+
+        StudentInfo.display();
+        //int width, height;
+
+        //Your code here
+        if (width == null || height == null) 
+        {
+            int width = 3;
+            int height = 3;
+        }
+
+        ArrayList<Solution> solutions = solve (width, height);
+
+        System.out.println ("The number of solutions is: "+solutions.size());
+        for (int i = 0; i < solutions.size(); i++)
+        {
+            System.out.println ("Solution "+i+1);
+            System.out.println (solutions.get(i));
+        }
+
+    }
+}
---
+++ 01/Solution.java
@@ -0,0 +1,218 @@
+
+/**
+ * The class <b>Solution</b> is used
+ * to store a (partial) solution to the game
+ *
+ * @ GVJ, Hrithik, Soorya all from uOttawa
+ */
+public class Solution {
+
+
+    // Your variables here
+    boolean [][] solHolder;
+    int iRow = 0;
+    int iCol = 0;
+    boolean ready = false;
+    int [][] selHolder;
+
+    /**
+     * Constructor. Creates an instance of Solution 
+     * for a board of size <b>widthxheight</b>. That 
+     * solution does not have any board position
+     * value explicitly specified yet.
+     *
+     * @param width
+     *  the width of the board
+     * @param height
+     *  the height of the board
+     */
+    public Solution(int width, int height) {
+
+        //Your code here
+        solHolder = new boolean [height][width];
+        selHolder = new int [height][width];
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+                selHolder[i][j] = 0;
+        }
+    }
+
+   /**
+     * Constructor. Creates an instance of Solution 
+     * wich is a deep copy of the instance received
+     * as parameter. 
+     *
+     * @param other
+     *  Instance of solution to deep-copy
+     */
+     public Solution(Solution other) {
+
+        //Your code here
+        this.solHolder = new boolean [other.solHolder.length][other.solHolder[0].length];
+        for (int i = 0; i < other.solHolder.length; i++)
+        {
+            for (int j = 0; j < other.solHolder[0].length; j++)
+                this.solHolder[i][j] = other.solHolder[i][j];
+        }
+    }
+
+
+    /**
+     * returns <b>true</b> if and only the parameter 
+     * <b>other</b> is referencing an instance of a 
+     * Solution which is the ``same'' as  this 
+     * instance of Solution (its board as the same
+     * values and it is completed to the same degree)
+     *
+     * @param other
+     *  referenced object to compare
+     */
+
+    public boolean equals(Object other){
+
+        //Your code here
+        if (other == null || other.getClass() != this.getClass()) {
+            return false;
+        }
+
+        Solution another;
+        another = (Solution) other;
+
+        for (int i = 0; i < this.solHolder.length; i++)
+        {
+            for (int j = 0; j < this.solHolder[i].length; j++)
+            {
+                if (this.solHolder[i][j] != another.solHolder[i][j])
+                    return false;
+            } 
+        }
+        return true;
+    }
+
+
+    /** 
+    * returns <b>true</b> if the solution 
+    * has been entirely specified
+    *
+    * @return
+    * true if the solution is fully specified
+    */
+    public boolean isReady(){
+
+        //Your code here
+        return ready;
+    }
+
+    /** 
+    * specifies the ``next'' value of the 
+    * solution. 
+    * The first call to setNext specifies 
+    * the value of the board location (1,1), 
+    * the second call specifies the value
+    *  of the board location (1,2) etc. 
+    *
+    * If <b>setNext</b> is called more times 
+    * than there are positions on the board, 
+    * an error message is printed out and the 
+    * call is ignored.
+    *
+    * @param nextValue
+    *  the boolean value of the next position
+    *  of the solution
+    */
+    public void setNext(boolean nextValue) {
+
+        //Your code here
+        if (ready == false)
+        {
+            if (iRow < solHolder.length)
+            {
+                if (iCol < solHolder[iRow].length)
+                {
+                    solHolder[iRow][iCol] = nextValue;
+                    if (nextValue == true)
+                    {
+                        selHolder[iRow][iCol]++;
+                        if (iRow > 0)
+                            selHolder[iRow-1][iCol]++;
+                        if (iRow < solHolder.length - 1)
+                            selHolder[iRow+1][iCol]++;
+                        if (iCol > 0)
+                            selHolder[iRow][iCol-1]++;
+                        if (iCol < solHolder[iRow].length - 1)
+                            selHolder[iRow][iCol+1]++; 
+                    }
+
+                    iCol++;
+                    if (iCol == solHolder[iRow].length)
+                    {
+                        iCol = 0;
+                        iRow++;
+                    }
+                }
+            }
+        }
+        else
+            System.out.println ("Solution is already ready.");
+        
+        if (iRow == solHolder.length)
+        {
+            ready = true;
+        }
+    }
+    
+    /**
+    * returns <b>true</b> if the solution is completely 
+    * specified and is indeed working, that is, if it 
+    * will bring a board of the specified dimensions 
+    * from being  entirely ``off'' to being  entirely 
+    * ``on''.
+    *
+    * @return
+    *  true if the solution is completely specified
+    * and works
+    */
+    public boolean isSuccessful(){
+
+        //Your code here
+        for (int i = 0; i < selHolder.length; i++)
+        {
+            for (int j = 0; j < selHolder[i].length; j++)
+            {
+                if (selHolder[i][j] % 2 == 0)
+                    return false;
+            } 
+        }
+        return true;
+    }
+
+
+    /**
+     * returns a string representation of the solution
+     *
+     * @return
+     *      the string representation
+     */
+    public String toString() {
+ 
+        //Your code here
+        String toreturn = "[[";
+        for (int i = 0; i < solHolder.length; i++)
+        {
+            for (int j = 0; j < solHolder[i].length; j++)
+            {
+                toreturn += solHolder[i][j];
+                toreturn += selHolder[i][j];
+                if (j != solHolder[i].length-1)
+                    toreturn += ", ";
+            } 
+            if (i != solHolder.length-1)
+                toreturn += "],\n";
+        }
+        toreturn += "]]";
+
+        return toreturn;
+    }
+
+}
